9ee5759c4b4c0a9a3a5b069e45a4b46d3e477c73
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-12-17 16:38:07 +0100

    animation added

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let audio = new Audio("pirates.mp3");
+const startBtn = document.getElementById("startBtn");
 let videoElem = document.getElementById("video");
 
 function playSong() {
   audio.play();
   videoElem.style.animation = "showVideo 1s 0s linear forwards";
   videoElem.play();
+  setTimeout(() => {
+    startBtn.classList.remove("hide");
+  }, 12000);
 }
