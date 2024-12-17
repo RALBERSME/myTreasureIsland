6fef8dfe71e34bc9ae12682fd859fdf126a9ecf8
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-12-17 14:58:36 +0100

    video + audio added

---
+++ main.js
@@ -0,0 +1,11 @@
+let audio = new Audio("pirates.mp3");
+let videoElem = document.getElementById("video");
+function playSong() {
+  audio.play();
+}
+
+function playSong() {
+  audio.play();
+  videoElem.style.animation = "showVideo 1s 0s linear forwards";
+  videoElem.play();
+}
